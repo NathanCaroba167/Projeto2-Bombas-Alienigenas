0ccd6a3e18f502268402d56e61fdfce622d842b4
Nathan Caroba <nathancaroba01@gmail.com>
2025-12-04 22:21:01 -0300

    feat(svg): adiciona .h e.c com implementação

---
+++ src/svg.c
@@ -0,0 +1,260 @@
+//
+// Created by natha on 04/10/2025.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <errno.h>
+
+#include "circulo.h"
+#include "retangulo.h"
+#include "linha.h"
+#include "texto.h"
+#include "segmento.h"
+#include "forma.h"
+
+#include "lista.h"
+#include "txt.h"
+#include "svg.h"
+
+#include "poligono.h"
+
+Arquivo abrirSVG(Nome arquivo) {
+    Arquivo svg = fopen(arquivo,"w");
+    if (svg == NULL) {
+        printf("Erro ao abrir o arquivo svg!\n");
+
+        perror("Motivo do erro");
+        exit(1);
+    }
+    return svg;
+}
+
+void inicializarSVG(Arquivo svg) {
+    if (svg == NULL) return;
+    fprintf(svg,"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n");
+    fprintf(svg,"<svg xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\" width=\"1000\" height=\"1000\">\n");
+    fprintf(svg,"\t<g>\n");
+}
+
+void desenharCirculoSVG(Arquivo svg, Circulo c) {
+    if (svg == NULL || c == NULL) return;
+    fprintf(svg, "<circle id=\"%d\" "
+                 "cx=\"%lf\" "
+                 "cy=\"%lf\" "
+                 "r=\"%lf\" "
+                 "stroke=\"%s\" "
+                 "fill=\"%s\" "
+                 "opacity=\"%lf\" "
+                 "stroke-width=\"%lf\" "
+                 "/>\n",
+                 getIDCirculo(c),
+                 getXCirculo(c),
+                 getYCirculo(c),
+                 getRCirculo(c),
+                 getCorBCirculo(c),
+                 getCorPCirculo(c),
+                 0.5,
+                 1.5);
+}
+
+void desenharRetanguloSVG(Arquivo svg, Retangulo r) {
+    if (svg == NULL || r == NULL) return;
+    fprintf(svg, "\t<rect id=\"%d\" "
+                 "x=\"%lf\" "
+                 "y=\"%lf\" "
+                 "width=\"%lf\" "
+                 "height=\"%lf\" "
+                 "stroke=\"%s\" "
+                 "fill=\"%s\" "
+                 "opacity=\"%lf\" "
+                 "stroke-width=\"%lf\" "
+                 "/>\n",
+                 getIDRetangulo(r),
+                 getXRetangulo(r),
+                 getYRetangulo(r),
+                 getWRetangulo(r),
+                 getHRetangulo(r),
+                 getCorBRetangulo(r),
+                 getCorPRetangulo(r),
+                 0.5,
+                 1.5);
+
+}
+
+void desenharLinhaSVG(Arquivo svg, Linha l) {
+    if (svg == NULL || l == NULL) return;
+    fprintf(svg, "\t<line id=\"%d\" "
+                 "x1=\"%lf\" "
+                 "y1=\"%lf\" "
+                 "x2=\"%lf\" "
+                 "y2=\"%lf\" "
+                 "stroke=\"%s\" "
+                 "stroke-width=\"%lf\" "
+                 "/>\n",
+                 getIDLinha(l),
+                 getX1Linha(l),
+                 getY1Linha(l),
+                 getX2Linha(l),
+                 getY2Linha(l),
+                 getCorLinha(l),
+                 1.5);
+
+}
+
+void desenharTextoSVG(Arquivo svg, Texto t, Estilo e) {
+    if (svg == NULL || t == NULL || e == NULL) return;
+    fprintf(svg, "\t<text id=\"%d\" "
+                 "x=\"%lf\" "
+                 "y=\"%lf\" "
+                 "stroke=\"%s\" "
+                 "fill=\"%s\" "
+                 "font-family=\"%s\" "
+                 "font-weight=\"%s\" "
+                 "font-size=\"%spt\" ",
+                 getIDTexto(t),
+                 getXTexto(t),
+                 getYTexto(t),
+                 getCorBTexto(t),
+                 getCorPTexto(t),
+                 getFontFamilyTexto(e),
+                 getFontWeightTexto(e),
+                 getFontSizeTexto(e));
+
+    switch (getATexto(t)) {
+        case 'i':
+            fprintf(svg, "text-anchor=\"start\" ");
+            break;
+        case 'm':
+            fprintf(svg, "text-anchor=\"middle\" ");
+            break;
+        case 'f':
+            fprintf(svg, "text-anchor=\"end\" ");
+            break;
+        default:
+            printf("Indice de ancora '%c' incorreto!\n",getATexto(t));
+            break;
+    }
+    fprintf(svg, ">%s</text>\n", getTxtoTexto(t));
+
+}
+
+void desenharSegmentoSVG(Arquivo svg, Segmento s) {
+    if (svg == NULL || s == NULL) return;
+    TipoSegmento tipo = getTipoSegmento(s);
+    if (tipo == AREA) {
+        fprintf(svg, "\t<line id=\"%d\" "
+                 "x1=\"%lf\" "
+                 "y1=\"%lf\" "
+                 "x2=\"%lf\" "
+                 "y2=\"%lf\" "
+                 "stroke=\"%s\" "
+                 "stroke-dasharray=\"5,5\" "
+                 "stroke-width=\"%lf\" "
+                 "/>\n",
+                 getIDSegmento(s),
+                 getX1Segmento(s),
+                 getY1Segmento(s),
+                 getX2Segmento(s),
+                 getY2Segmento(s),
+                 getCorSegmento(s),
+                 1.2);
+
+    }
+    fprintf(svg, "\t<line id=\"%d\" "
+                 "x1=\"%lf\" "
+                 "y1=\"%lf\" "
+                 "x2=\"%lf\" "
+                 "y2=\"%lf\" "
+                 "stroke=\"%s\" "
+                 "stroke-width=\"%lf\" "
+                 "/>\n",
+                 getIDSegmento(s),
+                 getX1Segmento(s),
+                 getY1Segmento(s),
+                 getX2Segmento(s),
+                 getY2Segmento(s),
+                 getCorSegmento(s),
+                 1.5);
+
+}
+
+void desenharBombaSVG(Arquivo svg, double x, double y) {
+    if (svg == NULL) return;
+    fprintf(svg, "\t<text x=\"%lf\" "
+                 "y=\"%lf\" "
+                 "fill=\"red\" "
+                 "font-size=\"%d\" "
+                 ">*</text>\n",
+                 x,
+                 y,
+                 18);
+}///
+
+void desenharPoligonoSVG(Arquivo svg, Poligono p) {
+    if (svg == NULL || p == NULL) return;
+
+    fprintf(svg, "<polygon points=\"");
+    Lista pontos = getListaPontosPoligono(p);
+
+    pont atual = getPrimeiroElementoLista(pontos);
+    while (atual != NULL) {
+        Ponto pt = getPacoteElementoLista(atual);
+        fprintf(svg, "%lf,%lf ", getXPonto(pt), getYPonto(pt));
+        atual = getProximoElementoLista(pontos);
+    }
+
+    fprintf(svg, "\" fill=\"yellow\" opacity=\"0.5\" stroke=\"none\" />\n");
+}
+
+void desenharFormasDaLista(Arquivo svg, Lista formas, Estilo EstiloGlobalTexto) {
+    if (svg == NULL || formas == NULL || listavazia(formas)) {
+        exit(1);
+    }
+
+    pont atual = getPrimeiroElementoLista(formas);
+
+    while (atual != NULL) {
+        Pacote p = getPacoteElementoLista(atual);
+
+        TipoForma tipo = getTipoForma(p);
+        Forma forma = getDadosForma(p);
+
+        switch (tipo) {
+            case CIRCULO:
+                desenharCirculoSVG(svg, forma);
+                break;
+            case RETANGULO:
+                desenharRetanguloSVG(svg, forma);
+                break;
+            case LINHA:
+                desenharLinhaSVG(svg, forma);
+                break;
+            case TEXTO:
+                desenharTextoSVG(svg, forma, EstiloGlobalTexto);
+                break;
+            case SEGMENTO:
+                desenharSegmentoSVG(svg, forma);
+            default:
+                printf("ERRO: tipo inválido!\n");
+                break;
+        }
+
+        atual = getProximoElementoLista(atual);
+    }
+}
+
+void fecharSVG(Arquivo svg) {
+    if (svg == NULL) return;
+    fprintf(svg, "</g>\n");
+    fprintf(svg,"</svg>\n");
+
+    printf("Arquivo SVG gerado com sucesso!\n");
+
+}
+
+void gerarSVG(Lista formas, Arquivo arqSvg,Estilo EstiloGlobalTexto) {
+    desenharFormasDaLista(arqSvg, formas,EstiloGlobalTexto);
+    fecharSVG(arqSvg);
+}
---
+++ src/svg.h
@@ -0,0 +1,88 @@
+//
+// Created by natha on 04/10/2025.
+//
+
+#ifndef SVG_H
+#define SVG_H
+
+/*
+ Módulo encarregado de gerar um arquivo SVG a partir das formas presentes e executar os comandos com as operações de abrir,
+ inicializar, desenhar, gerar e fechar
+ */
+
+/*
+ Ponteiro do tipo FILE Arquivo
+ Ponteiro char Nome
+ */
+typedef FILE* Arquivo;
+typedef char* Nome;
+
+/// @brief Cria e abre o arquivo SVG para escrita (sobrescrevendo o conteúdo existentes).
+/// @param arquivo Ponteiro para o nome do arquivo que será criado e aberto.
+/// @return Ponteiro para o arquivo (FILE*).
+/// @warning
+Arquivo abrirSVG(Nome arquivo);
+
+/// @brief Adiciona a tag iniciais do arquivo SVG.
+/// @param svg Ponteiro para o arquivo que será inicializado.
+/// @warning
+void inicializarSVG(Arquivo svg);
+
+/// @brief Desenha um círculo no arquivo SVG.
+/// @param svg Ponteiro para o arquivo que receberá o desenho.
+/// @param c Círculo que será desenhado.
+/// @warning
+void desenharCirculoSVG(Arquivo svg, Circulo c);
+
+/// @brief Desenha um retângulo no arquivo SVG.
+/// @param svg Ponteiro para o arquivo que receberá o desenho.
+/// @param r Retângulo que será desenhado.
+/// @warning
+void desenharRetanguloSVG(Arquivo svg, Retangulo r);
+
+/// @brief Desenha uma linha no arquivo SVG.
+/// @param svg Ponteiro para o arquivo que receberá o desenho.
+/// @param l Linha que será desenhada.
+/// @warning
+void desenharLinhaSVG(Arquivo svg, Linha l);
+
+/// @brief Desenha um texto no arquivo SVG.
+/// @param svg Ponteiro para o arquivo que receberá o desenho.
+/// @param t Texto que será desenhado.
+/// @param e Estilo do texto que será desenhado.
+/// @warning
+void desenharTextoSVG(Arquivo svg, Texto t, Estilo e);
+
+/// @brief Desenha um segmento no arquivo SVG.
+/// @param svg Ponteiro para o arquivo que receberá o desenho.
+/// @param s Segmento que será desenhado.
+/// @warning
+void desenharSegmentoSVG(Arquivo svg, Segmento s);
+
+/// @brief Desenha um asterisco vermelho no local da bomba lançada.
+/// @param svg Ponteiro para o arquivo que receberá o desenho.
+/// @param x Coordenada x da bomba.
+/// @param y Coordenada y da bomba.
+/// @warning
+void desenharBombaSVG(Arquivo svg, double x, double y);///
+
+/// @brief Desenha todas as formas presentes na lista da área no arquivo SVG.
+/// @param svg Ponteiro para o arquivo SVG que receberá o desenho.
+/// @param forma Lista que contém as formas.
+/// @param EstiloGlobalTexto Estilo global do texto.
+/// @warning
+void desenharFormasDaLista(Arquivo svg, Lista forma,Estilo EstiloGlobalTexto);
+
+/// @brief Fecha o arquivo SVG, adicionando as tags de fechamento.
+/// @param svg Ponteiro para o arquivo que será fechado.
+/// @warning
+void fecharSVG(Arquivo svg);
+
+/// @brief Gera arquivo SVG após a leitura da lista e fecha o arquivo.
+/// @param forma Lista que mandará as formas.
+/// @param arqSvg Ponteiro para o arquivo SVG.
+/// @param EstiloGlobalTexto Estilo global do texto.
+/// @warning
+void gerarSVG(Lista forma, Arquivo arqSvg, Estilo EstiloGlobalTexto);
+
+#endif //SVG_H
