c038ad230f80a8234aabfe35b2ad356114c744dc
Nathan Caroba <nathancaroba01@gmail.com>
2025-12-11 11:31:14 -0300

    fix(visibilidade): documentação de .h

--- src/visibilidade.h
+++ src/visibilidade.h
@@ -13,6 +13,29 @@
  Módulo encarregado de calcular um poligono que representa a região de visibilidade da bomba lançada.
  */
 
+/// @brief Calcula o ponto onde o eixo X da bomba (ângulo 0°) corte o anteparo.
+/// @param s Ponteiro para o segmento analisado.
+/// @param theta Ângulo para analise.
+/// @param xSaida Coordenada x da interseccao no anteparo.
+/// @param ySaida Coordenada y da interseccao no anteparo.
+/// @return Retorna uma validação de intersecção 1 há interseccao 0 não há.
+/// @warning
+int calcularInterseccaoRaio(Segmento s, double theta, double* xSaida, double* ySaida);
+
+/// @brief Compara Segmentos para verificação da árvore.
+/// @param a Segmento a analisado.
+/// @param b Segmento b analisado.
+/// @return Inteiro que indica a direção na árvore que o segmento deve ficar.
+/// @warning
+int comparaSegmentosArvore(Segmento a, Segmento b);
+
+/// @brief Trata o início da leitura da região de visibilidade.
+/// @param anteparos Ponteiro para a lista de anteparos.
+/// @param bx Coordenada x da queda da bomba.
+/// @param by Coordenada y da queda da bomba.
+/// @warning
+void tratarSegmentosQueCruzamInicio(Lista anteparos, double bx, double by);
+
 /// @brief Calcula a região de visibilidade da bomba.
 /// @param anteparos Ponteiro para lista de anteparos analisada.
 /// @param bx Coordenada x da queda da bomba.
@@ -22,11 +45,5 @@
 /// @return Ponteiro para o poligono/região de visibilidade da bomba.
 /// @warning
 Poligono calcularRegiaoVisibilidade(Lista anteparos, double bx, double by, char tipoOrd, int limIns);
-
-int comparaSegmentosArvore(Segmento a, Segmento b);
-
-void tratarSegmentosQueCruzamInicio(Lista anteparos, double bx, double by);
-
-int calcularInterseccaoRaio(Segmento s, double theta, double* xSaida, double* ySaida);
 
 #endif //VISIBILIDADE_H
